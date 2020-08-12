af4ba041bd52c5255a093211da5ddbbdc85a3767
yoonsangkwak <yunsang08@naver.com>
2020-08-12 17:06:13 +0900

    update day 7

--- day 7/src/index.js
+++ day 7/src/index.js
@@ -4,16 +4,19 @@
 let slider = document.querySelector("#slider")
 
 function whoWin() {
-    let your = Number(document.getElementById("choice").value);
+    let your = document.getElementById("choice").value;
     let computer = Math.round(Math.random() * slider.value);
     let battle = document.querySelector(".battle");
     let result = document.querySelector(".result");
-    if (your === computer) {
+    if (Number(your) === computer) {
         battle.innerText = "You choose: " + your + ", machine choose: " + computer;
         result.innerText = "You Won!";
-    } else if (your < 0 || your > slider.value) {
+    } else if (Number(your) < 0 || Number(your) > slider.value) {
         battle.innerText = "Are you kidding me?"
         result.innerText = "Out of range!";
+    } else if (your == "") {
+        battle.innerText = "Are you kidding me?"
+        result.innerText = "Come on!";
     } else {
         battle.innerText = "You choose: " + your + ", machine choose: " + computer;
         result.innerText = "You Lose!";
