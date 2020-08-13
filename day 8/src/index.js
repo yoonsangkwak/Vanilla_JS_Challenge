aaa6e17ca97a74cd545b945b385135d5dfde3427
yoonsangkwak <yunsang08@naver.com>
2020-08-13 16:58:44 +0900

    day 8
    wip

--- day 8/src/index.js
+++ day 8/src/index.js
@@ -2,15 +2,18 @@
 // import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-const reset = document.querySelector(".reset");
 const numbers = document.querySelectorAll(".num");
-const zeroNum = document.querySelector(".zero-num");
 const symbols = document.querySelectorAll(".symbol");
+const zeroNum = document.querySelector(".zero-num");
+const answer = document.querySelector(".answer");
+const reset = document.querySelector(".reset");
 
+pocket = 0;
 
 numbers.forEach(function(target) {
     target.addEventListener("mousedown", msDown);
     target.addEventListener("mouseup", msUp);
+    target.addEventListener("mouseup", output)
 });
 symbols.forEach(function(target) {
     target.addEventListener("mousedown", msDown);
@@ -28,4 +31,44 @@ function msDown(e) {
 function msUp(e) {
     val = e.target.id
     document.getElementById(val).style.opacity = 1.0;
+}
+
+function output(e) {
+    temp = e.target.innerText;
+    answer.innerText += temp;
+}
+
+document.querySelector("#plus").addEventListener("mouseup", add);
+function add() {
+    pocket += answer.innerText;
+    answer.innerText = 0;
+}
+
+document.querySelector("#minus").addEventListener("mouseup", substract);
+function substract() {
+    pocket -= answer.innerText;
+    answer.innerText = 0;
+}
+
+document.querySelector("#multiple").addEventListener("mouseup", multiple);
+function multiple() {
+    pocket *= answer.innerText;
+    answer.innerText = 0;
+}
+
+document.querySelector("#division").addEventListener("mouseup", divide);
+function divide() {
+    pocket /= answer.innerText;
+    answer.innerText = 0;
+}
+
+document.querySelector("#equal").addEventListener("mouseup", equal);
+function equal() {
+    answer.innerText = pocket;
+}
+
+document.querySelector("#clear").addEventListener("mouseup", clear);
+function clear() {
+    answer.innerText = 0;
+    pocket = 0;
 }
