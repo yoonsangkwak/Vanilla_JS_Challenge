8487070b2278d96b8ff815620658f90beae1c7d1
yoonsangkwak <yunsang08@naver.com>
2020-08-13 14:58:45 +0900

    day 8
    wip

---
+++ day 8/src/index.js
@@ -0,0 +1,14 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+// import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const reset = document.querySelector(".reset");
+const num = document.querySelector(".num");
+const zeroNum = document.querySelector(".zero-num");
+const symbol = document.querySelector(".symbol");
+
+num.addEventListener("onmousedown", )
+
+function boxShadow() {
+    
+}
