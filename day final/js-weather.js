0ebd66527b545b19e2ee70760604991830dad4e2
yoonsangkwak <yunsang08@naver.com>
2020-08-15 21:25:20 +0900

    day final
    demo

--- day final/js-weather.js
+++ day final/js-weather.js
@@ -1,6 +1,8 @@
 const API_KEY = "39b21a1bb381628bfa14045db5d8ea84";
 const COORDS = "coords";
 const weather = document.querySelector(".js-weather");
+const weatherMin = document.querySelector(".weather-min");
+const weatherMax = document.querySelector(".weather-max");
 
 
 function getWeather(lat, lng) {
@@ -11,7 +13,11 @@ function getWeather(lat, lng) {
     .then(function (json) {
         const temperature = json.main.temp;
         const place = json.name;
+        const tempMin = json.main.temp_min;
+        const tempMax = json.main.temp_max;
         weather.innerText = `${temperature}℃ at ${place}`
+        weatherMin.innerText = "최저 온도: " + tempMin + "℃";
+        weatherMax.innerText = "최고 온도: " + tempMax + "℃";
     })
 }
 
@@ -47,6 +53,15 @@ function loadCoords() {
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
+
+const weatherDiv = document.querySelector(".weather-click");
+const weatherIcon = document.querySelector(".weather-icon");
+function weatherMore() {
+    weatherIcon.classList.toggle("active");
+    weatherMin.classList.toggle("active");
+    weatherMax.classList.toggle("active");
+}
+weatherDiv.addEventListener("click", weatherMore);
 
 function init() {
     loadCoords();
